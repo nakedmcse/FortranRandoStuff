fa6e7021b1efe5b30b65ba96af8174cede874ed9
Walker Aldridge <daldridge@lairdscomputer.com>
2024-06-18 23:19:07 -0500

    Collatz in Fortran

---
+++ collatz.f90
@@ -0,0 +1,26 @@
+program for_collatz
+    implicit none
+    integer :: seed
+
+    do seed=0,1000000000
+        print *, "Seed:", seed, "Steps:", collatz(seed)
+    end do
+
+    contains
+        function collatz(oseed) result (csteps)
+            integer, intent(in) :: oseed
+            integer :: csteps, cseed
+            cseed = oseed
+            csteps = 0
+            do while(cseed > 1)
+                do while(mod(cseed,2) == 0)
+                    csteps = csteps + 1
+                    cseed = cseed / 2
+                end do
+                if(cseed > 1) then
+                    csteps = csteps + 1
+                    cseed = cseed * 3 + 1
+                end if
+            end do
+        end function collatz
+end program for_collatz
